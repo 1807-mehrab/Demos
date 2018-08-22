d7c50c1c1a0c38bef092b3a01d34c5977b861b03
unknown <mehrab.rahman@revature.com>
2018-08-22 11:36:26 -0500

    config

--- Week 8 MSA/CustomerService/src/main/java/com/revature/controller/CustomerController.java
+++ Week 8 MSA/CustomerService/src/main/java/com/revature/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.revature.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.model.Customer;
 
 @RestController
+@RefreshScope
 public class CustomerController {
 	private List<Customer> customers;
 	
